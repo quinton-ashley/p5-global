f3a5106814f72342c22c5c8b943166a2b878a2c0
Quinton Ashley <quinton-ashley@users.noreply.github.com>
2026-05-29 22:25:07 -0500

    1.0.0

--- p5-global.js
+++ p5-global.js
@@ -1,71 +1,64 @@
-
 window.P2D = 'p2d';
-window.P2D_HDR = 'p2d-hdr';
+window.P2DHDR = 'p2d-hdr';
 window.WEBGPU = 'webgpu';
 window.WEBGL = 'webgl';
-window.windowWidth = 'windowWidth';
-window.windowHeight = 'windowHeight';
+window.windowWidth = window.innerWidth;
+window.windowHeight = window.innerHeight;
 
-window.Canvas = (...args) => {
-  return new Promise((resolve) => {
-    window.setup = async function () {
-      const $ = p5.instance;
-
-      const entryPoints = [
-        "setup",
-        "update",
-        "draw",
-        "deviceMoved",
-        "deviceTurned",
-        "deviceShaken",
-        "doubleClicked",
-        "mousePressed",
-        "mouseReleased",
-        "mouseMoved",
-        "mouseDragged",
-        "mouseClicked",
-        "mouseWheel",
-        "touchStarted",
-        "touchMoved",
-        "touchEnded",
-        "keyPressed",
-        "keyReleased",
-        "keyTyped",
-        "windowResized",
-      ];
-
-      for (let ep of entryPoints) {
-        Object.defineProperty(p5, ep, {
-          set(fn) {
-            $[ep] = fn;
-            if ($._isGlobal) window[ep] = fn;
-          },
-          get() {
-            return $[ep];
-          },
-          configurable: true,
-        });
-      }
-      
-      let r = $.createCanvas(....map((arg) => {
-        if (arg == 'windowWidth') return windowWidth;
-        if (arg == 'windowHeight') return windowHeight;
-        return arg;
-      }));
-      if (r instanceof Promise) r = await r;
-      resolve(r);
-    };
-  });
+const runHook = p5.prototype._runLifecycleHook;
+p5.prototype._runLifecycleHook = async function (hookName) {
+	if (hookName == 'postsetup') return;
+	if (hookName == 'predraw' && !this._ranPostsetup) {
+		await runHook.call(this, 'postsetup');
+		this._ranPostsetup = true;
+	}
+	return runHook.call(this, hookName);
 };
 
-const runLifecycleHook = p5.prototype._runLifecycleHook;
-p5.prototype.isFirstDraw = true;
-p5.prototype._runLifecycleHook = async function(hookName) {
-  if (hookName === 'postsetup') {
-    return;
-  } else if (hookName === 'predraw' && this.isFirstDraw) {
-    await runLifecycleHook.call(this, 'postsetup');
-    this.isFirstDraw = false;
-  }
-  return runLifecycleHook.call(this, hookName);
-}
+window.Canvas = (...args) => {
+	return new Promise((resolve) => {
+		window.setup = async function () {
+			const $ = p5.instance;
+
+			const entryPoints = [
+				'setup',
+				'update',
+				'draw',
+				'deviceMoved',
+				'deviceTurned',
+				'deviceShaken',
+				'doubleClicked',
+				'mousePressed',
+				'mouseReleased',
+				'mouseMoved',
+				'mouseDragged',
+				'mouseClicked',
+				'mouseWheel',
+				'touchStarted',
+				'touchMoved',
+				'touchEnded',
+				'keyPressed',
+				'keyReleased',
+				'keyTyped',
+				'windowResized'
+			];
+
+			for (let ep of entryPoints) {
+				Object.defineProperty(p5, ep, {
+					set(fn) {
+						$[ep] = fn;
+						if ($._isGlobal) window[ep] = fn;
+					},
+					get() {
+						return $[ep];
+					},
+					configurable: true
+				});
+			}
+
+			let r = $.createCanvas(...args);
+			if (r instanceof Promise) r = await r;
+			resolve(r);
+		};
+	});
+};
